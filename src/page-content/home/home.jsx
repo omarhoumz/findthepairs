423b149800fe7e58f1b1af1ac97133e4f6280e2c
Omar Houmz <omarhoumz@gmail.com>
2020-10-26 21:33:55 +0100

    chore: show state in home page

--- src/page-content/home/home.jsx
+++ src/page-content/home/home.jsx
@@ -10,18 +10,27 @@ import {
 } from 'antd'
 import Head from 'next/head'
 import { memo } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Card from 'src/component/card/card'
+import generateCards from 'src/core/generate-cards'
 import { flipCard } from 'src/store/actions'
 import { range } from 'src/utils/range'
+
 import { Container, ContentGrid, Controls, Header } from './home.styled'
 
 const { Title } = Typography
 const { Content, Sider } = Layout
 const { Option } = Select
 
+function getImgUrl(id) {
+  return `/pairs/pair-${id}.jpg`
+}
+
 const Home = memo(function Home() {
+  const score = useSelector((state) => state.score)
+  const pairs = useSelector((state) => state.pairs)
+  const tries = useSelector((state) => state.tries)
   const dispatch = useDispatch()
 
   return (
@@ -36,7 +45,12 @@ const Home = memo(function Home() {
       </Head>
 
       <Header>
-        <button onClick={() => dispatch(flipCard())}>flip card</button>
+        <Space>
+          <button onClick={() => dispatch(flipCard())}>flip card</button>
+          <div>{pairs}</div>
+          <div>{score}</div>
+          <div>{tries}</div>
+        </Space>
       </Header>
 
       <Header>
@@ -47,9 +61,9 @@ const Home = memo(function Home() {
           <Col xs={22} md={14}>
             <Content>
               <Row gutter={16} justify="center">
-                {range(20, 1).map((i) => (
+                {generateCards(pairs).map((i) => (
                   <Col key={i}>
-                    <Card />
+                    <Card img={getImgUrl(i)} />
                   </Col>
                 ))}
               </Row>
@@ -62,17 +76,17 @@ const Home = memo(function Home() {
             >
               <Controls direction="vertical" size={24}>
                 <Title level={4}>Score</Title>
-                <Title level={2}>1 / 10</Title>
-                <Title level={5}>Tries: 5</Title>
+                <Title level={2}>{score} / 10</Title>
+                <Title level={5}>Tries: {tries}</Title>
                 <Divider />
                 <Title level={4}>Options</Title>
 
                 <Space size={20}>
                   <Title level={5}>Size</Title>
-                  <Select defaultValue={20} style={{ width: 120 }}>
+                  <Select defaultValue={pairs} style={{ width: 120 }}>
                     {range(21, 1).map((index) => (
-                      <Option key={index + 1} value={index + 1}>
-                        {index + 1} {index + 1 == 1 ? 'Pair' : 'Pairs'}
+                      <Option key={index} value={index}>
+                        {index} {index == 1 ? 'Pair' : 'Pairs'}
                       </Option>
                     ))}
                   </Select>
